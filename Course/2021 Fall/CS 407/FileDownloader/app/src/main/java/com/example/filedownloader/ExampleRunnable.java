6598f0128d246650c353d49948c30f77bde40d43
Yixing <irene.tu9569@gmail.com>
2021-10-10 00:55:36 -0500

    Finish

--- Course/2021 Fall/CS 407/FileDownloader/app/src/main/java/com/example/filedownloader/ExampleRunnable.java
+++
@@ -1,4 +0,0 @@
-package com.example.filedownloader;
-
-public class ExampleRunnable {
-}
--- Course/2021 Fall/CS 407/FileDownloader/app/src/main/java/com/example/filedownloader/MainActivity.java
+++ Course/2021 Fall/CS 407/FileDownloader/app/src/main/java/com/example/filedownloader/MainActivity.java
@@ -5,31 +5,90 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    class ExampleRunnable implements Runnable {
+        @Override
+        public void run() {
+            mockFileDownloader();
+        }
+    }
+
+
     private static final String TAG = "MainActivity";
+    private Button startButton;
+    private volatile boolean stopThread = false;
+    private TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.button);
+        message = (TextView)findViewById(R.id.textView);
+
     }
 
     public void mockFileDownloader() {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("Downloading...");
+            }
+        });
+
         for (int downloadProgress = 0; downloadProgress <= 100; downloadProgress=downloadProgress+10) {
-            Log.d(TAG, "Download Progress: " + downloadProgress + "%");
+            if(stopThread) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startButton.setText("Start");
+                        message.setText("");
+                    }
+                });
+                return;
+            }
+
+            //Log.d(TAG, "Download Progress: " + downloadProgress + "%");
+
+            int finalDownloadProgress = downloadProgress;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    message.setText("Download Progress: " + finalDownloadProgress + "%");
+                }
+            });
+
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("Start");
+                message.setText("");
+            }
+        });
     }
 
     public void startDownload(View view){
         //mockFileDownloader();
+        stopThread = false;
         ExampleRunnable runnable = new ExampleRunnable();
         new Thread(runnable).start();
+    }
+
+    public void stopDownload(View view){
+        stopThread = true;
     }
 }
